9c1c07e9bff907bd39996729d5e9d8645c05f7ce
braslyvm <braslymorales@gmail.com>
2025-11-07 03:30:39 -0600

    se agregan funciones

--- Programa/Reglas.pl
+++ Programa/Reglas.pl
@@ -1,5 +1,46 @@
+% Variables dinamica para gusrdar los procesos 
+% Donde esta el jugador 
+:- dynamic(jugador/1).
+% Los objetos
+:- dynamic(inventario/1).
 %Import
 :- consult('BC.pl').
+% Recorrido del jugador
+:- dynamic(camino/1).
+camino([]).
+inicio:-jugador(X), retractall(camino(_)),asserta(camino([X])).
+
+:-inicio.
+
+%Lista de objetos usando
+:- dynamic(listusando/1).
+listusando([]).
+
+
+%tomar(Objeto)
+tomar(Objeto):-jugador(X),objeto(Objeto,X),inventario(Tengo),retractall(inventario(_)),asserta(inventario([Objeto|Tengo])).
+
+
+%usar (Objeto)
+usar(Objeto):-inventario(Tengo),member(Objeto,Tengo),inventario(Mio) ,\+ member(Objeto, Mio),retractall(listusando(_)),asserta(listusando([Objeto|Mio])).
+
+%puedo_ir(Hacia).
+puedo_ir(Hacia):-jugador(Aqui),conectado(Aqui,Hacia),requiere(Ocupo,Hacia),inventario(Mio),member(Ocupo, Mio).
+
+%mover(Lugar).
+mover(Lugar):-jugador(Aqui),conectado(Aqui,Hacia),requiere(Ocupo,Hacia),listusando(Mio),member(Ocupo, Mio),camino(Micamino),retract(camino(Micamino)),asserta(camino([Lugar|Micamino])).
+
+%donde_esta(Objeto).
+donde_esta(Objeto,X) :- objeto(Objeto,X).
+
+%que tengo 
+que_tengo:-inventario(X),write(X).
+
+%lugar_visitados.
+lugar_visitados:- camino(Micamino), write(Micamino).
+
+
+
 
 
 
